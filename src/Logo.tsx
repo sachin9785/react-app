d72d8d79b4563babe4dbcd0f4c371e417c5e9300
Sachin3 Kumar <Sachin3.Kumar@lntinfotech.com>
2022-08-25 22:23:57 +0530

    feat: test

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -4,7 +4,7 @@ import logo from './logo.svg'
 const Logo = (): any => {
   return (
     <>
-      <div className="text">Display react3111</div>
+      <div className="text">Display react31113</div>
       <img src={logo} className="App-logo" alt="logo" />
     </>
   )
