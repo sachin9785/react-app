13ba473edf2be3f24f8614cdf4f56696b2ef96fb
Sachin3 Kumar <Sachin3.Kumar@lntinfotech.com>
2022-08-26 14:48:32 +0530

    test5

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import logo from "./logo.svg";
+import React from 'react'
+import logo from './logo.svg'
 
 const Logo = (): any => {
   return (
     <>
-      <div className="text">Display react123</div>
+      <div className="text">Display react1233</div>
       <img src={logo} className="App-logo" alt="logo" />
     </>
-  );
-};
+  )
+}
 
-export default Logo;
+export default Logo
